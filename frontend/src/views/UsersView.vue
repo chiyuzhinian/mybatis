a154989cfcc614c50a56055872b3babca7ffd7c9
chiyuzhinian <97819034+chiyuzhinian@users.noreply.github.com>
2026-06-16 12:38:09 +0800

    Add full Vue frontend (Vite) with routing and proxy

---
+++ frontend/src/views/UsersView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page">
+    <h1>User Management</h1>
+    <UserList />
+  </div>
+</template>
+
+<script>
+import UserList from '../components/UserList.vue'
+export default {
+  name: 'UsersView',
+  components: { UserList }
+}
+</script>
